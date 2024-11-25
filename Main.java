f76d4105ff725bf8b4cf91e04fa352d89f411234
bobfish234 <the.bobfish234@gmail.com>
2024-11-25 12:29:51 -0500

    Added Explanation
    added diagram to demonstrated gauss summation

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Gauss gausser = (int s, int e) -> ((e*e+e)/2-(s*s-s)/2);
+
+        /*
+
+        0
+        00
+        000
+        0000
+        11111   start
+        111111
+        1111111
+        11111111
+        111111111   end
+
+         */
+
         System.out.println(gausser.gauss(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
     }
 }
